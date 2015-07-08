5793663e00f027e55efd227e58f700ee1a050a10
Rebekah Chang <rchang@primerevenue.com>
2015-07-08 14:36:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuilDegree::Application.config.secret_token = '11262c33840e8f22d1da1f81668d91399dcd075386365cb79f02c23916ff053e0c821e57befb37156306635b4490578571d00796ad47e3b48dcbcb1f599be506'
